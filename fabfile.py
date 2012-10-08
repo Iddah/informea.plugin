009685665d0829148a1fe10de108d8c3317fff51
Cristian Romanescu <cristian.romanescu@eaudeweb.ro>
2012-10-08 14:22:26 +0300

    Updated fabric to 'one-click' wordpress upgrade

--- fabfile.py
+++ fabfile.py
@@ -13,155 +13,73 @@ To get specific command help type:
 """
 
 import os
-
+from datetime import datetime
 from fabric.api import *
 from fabric.utils import puts
 from fabric import colors
 import fabric.network
 import fabric.state
 
-
-JSON_AVAILABLE = True
-try:
-    import simplejson as json
-except ImportError:
-    try:
-        import json
-    except ImportError:
-        JSON_AVAILABLE = False
-
-################################
-#         ENVIRONMENTS         #
-################################
-
-class DictObj(object):
-    def __init__(self, d):
-        self.d = d
-
-    def __getattr__(self, m):
-        return self.d.get(m, None)
-
-def _load_config(**kwargs):
-    """Find and parse server config file.
-
-    """
-    config, ext = os.path.splitext(kwargs.get('config', 'config.json'))
-
-    if not os.path.exists(config + ext):
-        print colors.red('Error. "%s" file not found.' % (config + ext))
-        return {}
-    elif JSON_AVAILABLE and ext =='.json':
-        loader = json
-    else:
-        print colors.red('Parser package not available')
-        return {}
-    # Open file and deserialize settings.
-    with open(config + ext) as config_file:
-        return loader.load(config_file)
+cfg = {
+    'prod' : {
+        'host'          : 'www.informea.org',
+        'root'          : '/var/local/www.informea.org',
+        'url'           : 'http://www.informea.org',
+        'user'          : 'cristiroma',
+        'db_user'       : 'informea',
+        'db_password'   : 'dFzQksRAXYqWc1lOPsVd9i',
+        'db_database'   : 'informea',
+        'basedir'       : '/var/local/',
+        'backupdir'     : '/home/cristiroma/sql-backups'
+    },
+    'local' : {
+            'host'          : 'localhost',
+            'root'          : '/home/cristiroma/workspace/informea/www',
+            'url'           : 'http://informea.eaudeweb.ro',
+            'db_user'       : 'root',
+            'db_password'   : 'root',
+            'db_database'   : 'informea'
+    }
+}
 
 
-def s(*args, **kwargs):
-    """Set destination servers or server groups by comma delimited list of names"""
-    # Load config
-    config = _load_config(**kwargs)
-    servers = config['servers']
-    # If no arguments were recieved, print a message with a list of available configs.
-    if not args:
-        print 'No server name given. Available configs:'
-        for key in servers:
-            print colors.green('\t%s' % key)
-
-    # Create `group` - a dictionary, containing copies of configs for selected servers. Server hosts
-    # are used as dictionary keys, which allows us to connect current command destination host with
-    # the correct config. This is important, because somewhere along the way fabric messes up the
-    # hosts order, so simple list index incrementation won't suffice.
-    env.group = {}
-    # For each given server name
-    for name in args:
-        #  Recursive function call to retrieve all server records. If `name` is a group(e.g. `all`)
-        # - get it's members, iterate through them and create `group`
-        # record. Else, get fields from `name` server record.
-        # If requested server is not in the settings dictionary output error message and list all
-        # available servers.
-        _build_group(name, servers)
-
-
-    # Copy server hosts from `env.group` keys - this gives us a complete list of unique hosts to
-    # operate on. No host is added twice, so we can safely add overlaping groups. Each added host is
-    # guaranteed to have a config record in `env.group`.
-    env.hosts = env.group.keys()
-
-    # Set local as special server
-    setattr(env, 'local', DictObj(config['servers']['local']))
-
-def _build_group(name, servers):
-    """Recursively walk through servers dictionary and search for all server records."""
-    # We're going to reference server a lot, so we'd better store it.
-    server = servers.get(name, None)
-    # If `name` exists in servers dictionary we
-    if server:
-        # check whether it's a group by looking for `members`
-        if isinstance(server, list):
-            if fabric.state.output['debug']:
-                    puts("%s is a group, getting members" % name)
-            for item in server:
-                # and call this function for each of them.
-                _build_group(item, servers)
-        # When, finally, we dig through to the standalone server records, we retrieve
-        # configs and store them in `env.group`
+def _rec_setup(ob, properties):
+    for key, value in properties.items():
+        if isinstance(value, dict):
+            v = fabric.state._AttributeDict()
+            setattr(ob, key, v)
+            _rec_setup(v, value)
         else:
-            if fabric.state.output['debug']:
-                    puts("%s is a server, filling up env.group" % name)
-            env.group[server['host']] = server
-    else:
-        print colors.red('Error. "%s" config not found. Run `fab s` to list all available configs' % name)
+            setattr(ob, key, value)
 
-def _setup(task):
+    env.hosts.append(env.prod.host)
+    env.user = env.prod.user
+
+_rec_setup(env, cfg)
+
+#
+# Tasks
+#
+
+def prod_db_backup():
+    """ Backup the remote production database
     """
-    Copies server config settings from `env.group` dictionary to env variable.
+    filename = os.path.join(env.prod.backupdir,  '%s.sql.gz' % datetime.now().strftime("%Y%m%d"))
+    print colors.green('Backup production database to %s' % filename)
+    run("mysqldump -u %s --password=%s %s | gzip > %s" % (env.prod.db_user, env.prod.db_password, env.prod.db_database, filename))
 
-    This way, tasks have easier access to server-specific variables:
-        `env.owner` instead of `env.group[env.host]['owner']`
 
+def prod_update():
+    """ Update code on the production server
     """
-    def task_with_setup(*args, **kwargs):
-        # If `s:server` was run before the current command - then we should copy values to
-        # `env`. Otherwise, hosts were passed through command line with `fab -H host1,host2
-        # command` and we skip.
-        if env.get("group", None):
-            for key,val in env.group[env.host].items():
-                setattr(env, key, val)
-                if fabric.state.output['debug']:
-                    puts("[env] %s : %s" % (key, val))
-
-        task(*args, **kwargs)
-        # Don't keep host connections open, disconnect from each host after each task.
-        # Function will be available in fabric 1.0 release.
-        # fabric.network.disconnect_all()
-    return task_with_setup
-
-#############################
-#          TASKS            #
-#############################
-
-@_setup
-def db_backup():
-    """ TODO: Not implemented yet. Backup the remote production database
-    """
-    pass
-
-
-@_setup
-def update():
-    with cd(env.root):
+    with cd(env.prod.root):
         sudo("git pull origin master")
         sudo("./compress.sh css")
         sudo("./compress.sh js")
 
 
-@_setup
-def clonedb():
-    """ Clone SQL database from production to local
+def clone_to_local():
+    """ Clone production database to local
     """
     if not hasattr(env, 'local'):
         print colors.red('Usage fab s:prod clonedb')
@@ -197,30 +115,49 @@ def clonedb():
     print colors.red("Don't forget to disable CSS & JavaScript optimizations from back-end")
 
 
-@_setup
 def prod_update_wordpress():
     """ Update WordPress on production. Install into another base
         directory on production then manually move the synmlink to point
         to this new directory.
     """
     # 1. Create target folder
-    # 2. Download and unpack latest wordpress
-    # 3. Remove wp-content directory
-    # 4. Clone repository here
-    # 5. Run minification
-    # 6. Create symlinks to www.informea.org.resources (uploads, wp-content/uploads)
-    if not hasattr(env, 'basedir'):
-        print 'Usage fab prod_update_wordpress'
-        return
+    new_root = os.path.join(env.prod.basedir,  'www.informea.org.%s' % datetime.now().strftime("%Y%m%d"))
+    sudo('rm -rf %s' % new_root)
 
-    new_root = os.path.join(env.basedir, 'www.informea.org.%s' % date('Ymd'))
-    print new_root
-    pass
+    # 2. Clone the repository there
+    sudo('git clone /var/local/git/www.informea.org %s' % new_root)
+
+    # 3. Download and unpack latest wordpress
+    with cd(new_root):
+        sudo('wget http://wordpress.org/latest.tar.gz')
+        sudo('tar zxf latest.tar.gz')
+        sudo('rm -rf latest.tar.gz')
+
+        for f in ['wordpress/wp-content', 'wordpress/wp-config-sample.php', 'wordpress/readme.html', 'wordpress/license.txt']:
+            sudo('rm -rf %s' % f)
+        sudo('mv wordpress/* .')
+        sudo('rm -rf wordpress')
+        sudo('chown root:root * -R')
+        sudo('cp %s .' % os.path.join(env.prod.root, 'wp-config.php'))
+        sudo('chown root:apache wp-config.php')
+        sudo('chmod 440 wp-config.php')
+
+        # 4. Create symlinks to the dynamic resources
+        resdir = 'www.informea.org.resources'
+        for symlink in ['api-data', 'clickheat', 'uploads',
+                'wp-content/uploads',
+                'wp-content/plugins/informea/gis/countries.map']:
+            sudo('ln -s %s %s' % (os.path.join(env.prod.basedir, resdir, symlink), symlink))
+
+        # 5. Minimize JS/CSS
+        sudo('./compress.sh js')
+        sudo('./compress.sh css')
+
+    print colors.green('New InforMEA ready @: %s' % new_root)
 
 
-@_setup
 def shell():
-    """ Open a remote shell into drupal installation directory
+    """ Open a remote shell on production
     """
 
     from fabric.operations import open_shell
