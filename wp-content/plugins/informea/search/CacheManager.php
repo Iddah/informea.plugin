0ce6f71feba8302e0405bb0542b6b1bea1b0d5fd
Cristian Romanescu <cristian@romanescu.ro>
2012-10-11 16:47:54 +0300

    New cache content

--- wp-content/plugins/informea/search/CacheManager.php
+++ wp-content/plugins/informea/search/CacheManager.php
@@ -3,6 +3,9 @@
 class CacheManager {
 	protected static $cache_treaty_treatyparagraph = array();
 	protected static $cache_treaty_decisionparagraph = array();
+	protected static $cache_article_treaty = array();
+	protected static $cache_decision_treaty = array();
+	protected static $cache_decisiondocument_treaty = array();
 
 
     /**
@@ -17,12 +20,15 @@ class CacheManager {
     static function clear() {
         self::$cache_treaty_treatyparagraph = array();
         self::$cache_treaty_decisionparagraph = array();
+        self::$cache_article_treaty = array();
+        self::$cache_decision_treaty = array();
+        self::$cache_decisiondocument_treaty = array();
     }
 
 
     /**
      * Find treaty for a treaty paragraph
-     * @param type $id_paragraph ID of the treaty's paragraph to retrieve
+     * @param integer $id_paragraph ID of the treaty's paragraph to retrieve
      * @return integer ID of the treaty for this paragraph
      */
 	static function get_treaty_for_treaty_paragraph($id_paragraph) {
@@ -38,7 +44,7 @@ class CacheManager {
 
     /**
      * Find treaty for a decision paragraph
-     * @param type $id_paragraph ID of the decision's paragraph to retrieve
+     * @param integer $id_paragraph ID of the decision's paragraph to retrieve
      * @return integer ID of the treaty for this paragraph
      */
 	static function get_treaty_for_decision_paragraph($id_paragraph) {
@@ -51,6 +57,40 @@ class CacheManager {
             return array_key_exists($idx, self::$cache_treaty_decisionparagraph) ? self::$cache_treaty_decisionparagraph[$idx] : null;
         }
 	}
+
+
+    /**
+     * Find the treaty for an article
+     * @param integer $id_article
+     * @return integer ID of the treaty for this article
+     */
+    static function get_treaty_for_article($id_article) {
+        $idx = intval($id_article);
+        if($idx > 0) {
+            // Lazy load the cache
+            if(empty(self::$cache_article_treaty)) {
+                CacheManager::cache_treaties_for_articles();
+            }
+            return array_key_exists($idx, self::$cache_article_treaty) ? self::$cache_article_treaty[$idx] : null;
+        }
+    }
+
+
+    /**
+     * Find the treaty for an decision
+     * @param integer $id_decision
+     * @return integer ID of the treaty for this decision
+     */
+    static function get_treaty_for_decision($id_decision) {
+        $idx = intval($id_decision);
+        if($idx > 0) {
+            // Lazy load the cache
+            if(empty(self::$cache_decision_treaty)) {
+                CacheManager::cache_treaties_for_decisions();
+            }
+            return array_key_exists($idx, self::$cache_decision_treaty) ? self::$cache_decision_treaty[$idx] : null;
+        }
+    }
 
 
     private static function cache_treaties_for_treaties_paragraphs() {
@@ -72,6 +112,24 @@ class CacheManager {
         $rows = $wpdb->get_results($sql);
         foreach($rows as $row) {
             self::$cache_treaty_decisionparagraph[intval($row->id_paragraph)] = intval($row->id_treaty);
+        }
+    }
+
+    private static function cache_treaties_for_articles() {
+        global $wpdb;
+        $sql = 'SELECT id as id_article, id_treaty FROM ai_treaty_article';
+        $rows = $wpdb->get_results($sql);
+        foreach($rows as $row) {
+            self::$cache_article_treaty[intval($row->id_article)] = intval($row->id_treaty);
+        }
+    }
+
+    private static function cache_treaties_for_decisions() {
+        global $wpdb;
+        $sql = 'SELECT id as id_decision, id_treaty FROM ai_decision';
+        $rows = $wpdb->get_results($sql);
+        foreach($rows as $row) {
+            self::$cache_decision_treaty[intval($row->id_decision)] = intval($row->id_treaty);
         }
     }
 }
--- wp-content/plugins/informea/search/tests/CacheManagerTest.php
+++ wp-content/plugins/informea/search/tests/CacheManagerTest.php
@@ -13,12 +13,23 @@ class CacheManagerTestImpl extends CacheManager {
     public static function get_cache_treaty_decisionparagraph() {
         return parent::$cache_treaty_decisionparagraph;
     }
+
+    public static function get_cache_treaty_article() {
+        return parent::$cache_article_treaty;
+    }
+
+    public static function get_cache_treaty_decision() {
+        return parent::$cache_decision_treaty;
+    }
 }
 
 class CacheManagerTest extends WP_UnitTestCase {
 
 
     function test_get_treaty_for_treaty_paragraph_laziness() {
+
+        CacheManagerTestImpl::clear();
+
         $cache = CacheManagerTestImpl::get_cache_treaty_treatyparagraph();
         $this->assertEquals(array(), $cache);
 
@@ -33,8 +44,12 @@ class CacheManagerTest extends WP_UnitTestCase {
         $this->assertEquals(46, CacheManagerTestImpl::get_treaty_for_treaty_paragraph('7715'));
     }
 
+    ///
 
     function test_get_treaty_for_decision_paragraph_laziness() {
+
+        CacheManagerTestImpl::clear();
+
         $cache = CacheManagerTestImpl::get_cache_treaty_decisionparagraph();
         $this->assertEquals(array(), $cache);
 
@@ -47,5 +62,45 @@ class CacheManagerTest extends WP_UnitTestCase {
 
     function test_get_treaty_for_decision_paragraph() {
         $this->assertEquals(46, CacheManagerTestImpl::get_treaty_for_decision_paragraph('35'));
+    }
+
+    ///
+
+    function test_get_treaty_for_article_laziness() {
+
+        CacheManagerTestImpl::clear();
+
+        $cache = CacheManagerTestImpl::get_cache_treaty_article();
+        $this->assertEquals(array(), $cache);
+
+        $this->assertEquals(46, CacheManagerTestImpl::get_treaty_for_article('1405'));
+
+        $cache1 = CacheManagerTestImpl::get_cache_treaty_article();
+        $this->assertNotEquals(array(), $cache1);
+    }
+
+
+    function test_get_treaty_for_article() {
+        $this->assertEquals(46, CacheManagerTestImpl::get_treaty_for_article('1405'));
+    }
+
+    ///
+
+    function test_get_treaty_for_decision_laziness() {
+
+        CacheManagerTestImpl::clear();
+
+        $cache = CacheManagerTestImpl::get_cache_treaty_decision();
+        $this->assertEquals(array(), $cache);
+
+        $this->assertEquals(46, CacheManagerTestImpl::get_treaty_for_decision('10303'));
+
+        $cache1 = CacheManagerTestImpl::get_cache_treaty_decision();
+        $this->assertNotEquals(array(), $cache1);
+    }
+
+
+    function test_get_treaty_for_decision() {
+        $this->assertEquals(46, CacheManagerTestImpl::get_treaty_for_decision('10303'));
     }
 }
